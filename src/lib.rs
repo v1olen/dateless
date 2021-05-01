bd40176ea2261c1c1e388fea43fcd5db8ea071e8
v1olen <v1olen@v-sn.io>
2021-05-01 16:16:59 +0200

    Add docs for `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,91 @@
+#![doc(html_logo_url = "https://cdn.v-sn.io/dateless-logo")]
+
+/*!
+
+# Dateless
+
+Dateless is an events & calendar library for Rust.
+
+## Usage
+
+First, add `dateless` as a dependency in your project's Cargo.toml:
+
+```toml
+[dependencies]
+dateless = "0.2.0"
+```
+
+And then, you can start with creating a calendar:
+
+```rust
+use dateless::prelude::*;
+
+fn main() {
+    let calendar = Calendar::new();
+}
+```
+
+Now, let's create an `Event` and assign it to the newly created `Calendar` instance. It can be done simply with `EventPartial`:
+
+```rust
+use dateless::prelude::*;
+use chrono::Utc;
+
+fn main() {
+    let calendar = Calendar::new();
+
+    let event = EventPartial::new(String::from("Anne's birthday"))
+        .with_period(EventPeriod::WholeDays(Utc::today(), Utc::today()))
+        .with_cyclicity(EventCyclicity::EveryWeek)
+        .complete();
+
+    let calendar = calendar.add_event(event);
+}
+```
+
+Above, we created a new `Event` called "Anne's birthday" lasting all day today and set it to be recurring every week. In the last line we assigned the event to `calendar`.
+
+Finally, we can check a specific day for occurrences of events.
+
+
+```rust
+use dateless::prelude::*;
+use chrono::{Utc, Duration};
+
+fn main() {
+    let calendar = Calendar::new();
+
+    let event = EventPartial::new(String::from("Anne's birthday"))
+        .with_period(EventPeriod::WholeDays(Utc::today(), Utc::today()))
+        .with_cyclicity(EventCyclicity::EveryWeek)
+        .complete();
+
+    let calendar = calendar.add_event(event);
+
+    let seven_days_later = Utc::today() + Duration::days(7);
+
+    println!("{:#?}", calendar.day(seven_days_later));
+}
+```
+
+It prints to `stdout`:
+
+```json
+[
+    EventOccurance {
+        name: "Anne's birthday",
+        description: None,
+        period: WholeDays(
+            2021-05-08Z,
+            2021-05-08Z,
+        ),
+    },
+]
+```
+
+*/
+
+
 mod calendar;
 mod event;
 pub mod prelude;
@@ -7,3 +95,6 @@ mod test;
 
 #[macro_use]
 extern crate optional_struct;
+
+pub use calendar::Calendar;
+pub use event::{EventPartial, EventPeriod, EventCyclicity, EventOccurance};
