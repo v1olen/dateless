c2b150a6886c66b42bf53b880e6c114518c49c52
v1olen <v1olen@v-sn.io>
2021-05-01 21:41:00 +0200

    Remove needless additional module from tests

--- src/test.rs
+++ src/test.rs
@@ -1,56 +1,53 @@
-#[cfg(test)]
-mod tests {
-    use crate::prelude::*;
-    use chrono::{Duration, Utc};
+use crate::prelude::*;
+use chrono::{Duration, Utc};
 
-    #[test]
-    fn one_day_and_one_time_date() {
-        let mut calendar = Calendar::new();
+#[test]
+fn one_day_and_one_time_date() {
+    let mut calendar = Calendar::new();
 
-        let today = Utc::today();
-        let yesterday = today - Duration::days(1);
-        let tomorrow = today + Duration::days(1);
+    let today = Utc::today();
+    let yesterday = today - Duration::days(1);
+    let tomorrow = today + Duration::days(1);
 
-        let event = EventPartial::new(String::from("Date"))
-            .with_period(EventPeriod::WholeDays(today, today))
-            .complete();
+    let event = EventPartial::new(String::from("Date"))
+        .with_period(EventPeriod::WholeDays(today, today))
+        .complete();
 
-        calendar.add_event(event);
+    calendar.add_event(event);
 
-        let expected_occurrence = vec![EventOccurrence {
-            name: "Date".into(),
-            description: None,
-            period: EventPeriod::WholeDays(today, today),
-        }];
+    let expected_occurrence = vec![EventOccurrence {
+        name: "Date".into(),
+        description: None,
+        period: EventPeriod::WholeDays(today, today),
+    }];
 
-        assert_ne!(calendar.day(yesterday), expected_occurrence);
-        assert_eq!(calendar.day(today), expected_occurrence);
-        assert_ne!(calendar.day(tomorrow), expected_occurrence);
-    }
+    assert_ne!(calendar.day(yesterday), expected_occurrence);
+    assert_eq!(calendar.day(today), expected_occurrence);
+    assert_ne!(calendar.day(tomorrow), expected_occurrence);
+}
 
-    #[test]
-    fn one_hour_and_one_time_date() {
-        let mut calendar = Calendar::new();
+#[test]
+fn one_hour_and_one_time_date() {
+    let mut calendar = Calendar::new();
 
-        let today = Utc::today();
-        let yesterday = today - Duration::days(1);
-        let tomorrow = today + Duration::days(1);
+    let today = Utc::today();
+    let yesterday = today - Duration::days(1);
+    let tomorrow = today + Duration::days(1);
 
-        let (date_start, date_end) = (Utc::now(), Utc::now() + Duration::hours(1));
-        let event = EventPartial::new(String::from("Date"))
-            .with_period(EventPeriod::StartEnd(date_start, date_end))
-            .complete();
+    let (date_start, date_end) = (Utc::now(), Utc::now() + Duration::hours(1));
+    let event = EventPartial::new(String::from("Date"))
+        .with_period(EventPeriod::StartEnd(date_start, date_end))
+        .complete();
 
-        calendar.add_event(event);
+    calendar.add_event(event);
 
-        let expected_occurrence = vec![EventOccurrence {
-            name: "Date".into(),
-            description: None,
-            period: EventPeriod::StartEnd(date_start, date_end),
-        }];
+    let expected_occurrence = vec![EventOccurrence {
+        name: "Date".into(),
+        description: None,
+        period: EventPeriod::StartEnd(date_start, date_end),
+    }];
 
-        assert_ne!(calendar.day(yesterday), expected_occurrence);
-        assert_eq!(calendar.day(today), expected_occurrence);
-        assert_ne!(calendar.day(tomorrow), expected_occurrence);
-    }
+    assert_ne!(calendar.day(yesterday), expected_occurrence);
+    assert_eq!(calendar.day(today), expected_occurrence);
+    assert_ne!(calendar.day(tomorrow), expected_occurrence);
 }
