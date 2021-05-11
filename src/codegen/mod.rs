989f79713845ca3555b5f08e0c6f3e954fb629f7
v1olen <v1olen@v-sn.io>
2021-05-11 21:07:45 +0200

    Reimplement tests with macros

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,3 +1,87 @@
+#[cfg(test)]
+#[macro_export]
+macro_rules! test {
+    ($name:ident, $ne_items:expr) => {
+        #[test]
+        fn $name() {
+            use crate::{
+                event::{PeriodDef, WholeDays},
+                prelude::*,
+            };
+
+            use chrono::{Duration, Utc};
+
+            let today = Utc::today();
+
+            let event = EventPartial::new(String::from("Date"))
+                .whole_day(today)
+                .complete();
+
+            helper!(
+                event,
+                PeriodDef(Box::new(WholeDays(today, today))),
+                today,
+                $ne_items
+            );
+        }
+    };
+    ($name:ident, $period:ident, $period_struct:ident, $today_or_now:ident, $dur:expr, $ne_items:expr) => {
+        #[test]
+        fn $name() {
+            use crate::{event::PeriodDef, prelude::*};
+
+            use chrono::{Duration, Utc};
+
+            let moment = Utc::$today_or_now();
+
+            let event = EventPartial::new(String::from("Date"))
+                .$period(moment, moment + $dur)
+                .complete();
+
+            helper!(
+                event,
+                PeriodDef(Box::new(crate::event::$period_struct(
+                    moment,
+                    moment + $dur
+                ))),
+                moment,
+                $ne_items
+            );
+        }
+    };
+}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! helper {
+    ($event:expr, $period:expr, $day:expr, $ne_items:expr) => {
+        let event_uuid = $event.uuid.clone();
+
+        let mut calendar = Calendar::new();
+        calendar.add_event($event);
+
+        let expected_occurrence = serde_json::to_string(&vec![EventOccurrence {
+            origin: event_uuid,
+            name: "Date".into(),
+            description: None,
+            period: $period,
+        }])
+        .unwrap();
+
+        assert_eq!(
+            serde_json::to_string(&calendar.day(Utc::today())).unwrap(),
+            expected_occurrence,
+        );
+
+        for item in $ne_items.iter() {
+            assert_ne!(
+                serde_json::to_string(&calendar.day(Utc::today() + item.clone())).unwrap(),
+                expected_occurrence,
+            );
+        }
+    };
+}
+
 #[macro_export]
 macro_rules! bind_partial_filler {
     ($name:ident, $field:ident, $type:ident) => {
--- src/test.rs
+++ src/test.rs
@@ -1,129 +1,19 @@
-use crate::{
-    event::{PeriodDef, StartEnd, WholeDays},
-    prelude::*,
-};
+test!(one_day, [Duration::days(1), Duration::days(-1)]);
 
-use chrono::{DateTime, Duration, Utc};
+test!(
+    two_days,
+    whole_days,
+    WholeDays,
+    today,
+    Duration::days(1),
+    [Duration::days(2), Duration::days(-1)]
+);
 
-#[test]
-fn one_day_and_one_time_date() {
-    let mut calendar = Calendar::new();
-
-    let today = Utc::today();
-    let yesterday = today - Duration::days(1);
-    let tomorrow = today + Duration::days(1);
-
-    let event = EventPartial::new(String::from("Date"))
-        .whole_day(today)
-        .complete();
-
-    let event_uuid = event.uuid.clone();
-
-    calendar.add_event(event);
-
-    let expected_occurrence = serde_json::to_string(&vec![EventOccurrence {
-        origin: event_uuid,
-        name: "Date".into(),
-        description: None,
-        period: PeriodDef(Box::new(WholeDays(today, today))),
-    }])
-    .unwrap();
-
-    assert_ne!(
-        serde_json::to_string(&calendar.day(yesterday)).unwrap(),
-        expected_occurrence,
-    );
-    assert_eq!(
-        serde_json::to_string(&calendar.day(today)).unwrap(),
-        expected_occurrence,
-    );
-    assert_ne!(
-        serde_json::to_string(&calendar.day(tomorrow)).unwrap(),
-        expected_occurrence,
-    );
-}
-
-#[test]
-fn one_hour_and_one_time_date() {
-    let mut calendar = Calendar::new();
-
-    let today = Utc::today();
-    let yesterday = today - Duration::days(1);
-    let tomorrow = today + Duration::days(1);
-
-    let (date_start, date_end) = (Utc::now(), Utc::now() + Duration::hours(1));
-    let event = EventPartial::new(String::from("Date"))
-        .from_to(date_start, date_end)
-        .complete();
-
-    let event_uuid = event.uuid.clone();
-
-    calendar.add_event(event);
-
-    let expected_occurrence = serde_json::to_string(&vec![EventOccurrence {
-        origin: event_uuid,
-        name: "Date".into(),
-        description: None,
-        period: PeriodDef(Box::new(StartEnd(date_start, date_end))),
-    }])
-    .unwrap();
-
-    assert_ne!(
-        serde_json::to_string(&calendar.day(yesterday)).unwrap(),
-        expected_occurrence,
-    );
-    assert_eq!(
-        serde_json::to_string(&calendar.day(today)).unwrap(),
-        expected_occurrence,
-    );
-    assert_ne!(
-        serde_json::to_string(&calendar.day(tomorrow)).unwrap(),
-        expected_occurrence,
-    );
-}
-
-#[test]
-fn subtract_datetime_from_date() {
-    let mut calendar = Calendar::new();
-
-    let today = Utc::today();
-    let yesterday = today - Duration::days(1);
-    let tomorrow = today + Duration::days(1);
-    let two_days_later = today + Duration::days(2);
-
-    let date_start = DateTime::from_utc(today.naive_utc().and_hms(22, 59, 59), Utc);
-    let date_end = DateTime::from_utc(two_days_later.naive_utc().and_hms(0, 0, 0), Utc);
-
-    let event = EventPartial::new(String::from("Date"))
-        .from_to(date_start, date_end)
-        .complete();
-
-    let event_uuid = event.uuid.clone();
-
-    calendar.add_event(event);
-
-    let expected_occurrence = serde_json::to_string(&vec![EventOccurrence {
-        origin: event_uuid,
-        name: "Date".into(),
-        description: None,
-        period: PeriodDef(Box::new(StartEnd(date_start, date_end))),
-    }])
-    .unwrap();
-
-    assert_ne!(
-        serde_json::to_string(&calendar.day(two_days_later)).unwrap(),
-        expected_occurrence,
-    );
-    assert_eq!(
-        serde_json::to_string(&calendar.day(today)).unwrap(),
-        expected_occurrence,
-    );
-    assert_eq!(
-        serde_json::to_string(&calendar.day(tomorrow)).unwrap(),
-        expected_occurrence,
-    );
-    assert_ne!(
-        serde_json::to_string(&calendar.day(yesterday)).unwrap(),
-        expected_occurrence,
-    );
-}
+test!(
+    one_hour,
+    from_to,
+    StartEnd,
+    now,
+    Duration::hours(1),
+    [Duration::days(2), Duration::days(-1)]
+);
